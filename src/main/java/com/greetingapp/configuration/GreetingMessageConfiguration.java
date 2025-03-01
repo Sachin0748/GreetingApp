62a9d6824dc75f046bde8225b3fec1e3d769cc8d
Sachin Kumar Thakur <kumarsachinsk456@gmail.com>
2025-03-01 13:52:24 +0530

    [Sachin] add .GreetingApp UC_4

---
+++ src/main/java/com/greetingapp/configuration/GreetingMessageConfiguration.java
@@ -0,0 +1,14 @@
+package com.greetingapp.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GreetingMessageConfiguration {
+
+    @Bean
+    public ModelMapper getModelMapper(){
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/greetingapp/repo/GreetingRepository.java
@@ -0,0 +1,9 @@
+package com.greetingapp.repo;
+
+import com.greetingapp.GreetingApp.entities.GreetingMessageEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface GreetingRepository extends JpaRepository<GreetingMessageEntity, Integer> {
+}
